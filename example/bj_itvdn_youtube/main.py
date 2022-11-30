79c22c44dd2105d50a8e7e4f1b22cc0c7a39d0ca
vgalinskii <vgalinskii@turbosolution.ru>
2022-11-30 11:41:25 +0300

    from work

---
+++ example/bj_itvdn_youtube/main.py
@@ -0,0 +1,5 @@
+
+# пример придется собрать самому с видео
+# часть первая https://www.youtube.com/watch?v=i9Q95I5cbyA
+# часть вторая https://www.youtube.com/watch?v=CL-2D91k7F0
+
---
+++ main_bj.py
@@ -0,0 +1,9 @@
+
+# очко Black Jack по умному
+# тест колод
+# колоды использовать на 52 карты без джокеров
+koloda = [6,7,8,9,10,2,3,4,11] * 4
+import random
+random.shuffle(koloda)
+
+print(koloda)
